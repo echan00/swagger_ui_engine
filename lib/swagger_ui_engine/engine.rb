07361f5a870f60b59e81e16a00a9de8772ca1f7c
Erik Chan <erik.chan@gmail.com>
2020-04-28 15:35:17 +0800

    Update engine.rb

--- lib/swagger_ui_engine/engine.rb
+++ lib/swagger_ui_engine/engine.rb
@@ -12,6 +12,7 @@ module SwaggerUiEngine
         swagger_ui_engine/lib/print.css
         swagger_ui_engine/custom.css
         swagger_ui_engine/print.css
+        swagger_ui_engine/blue.css
         swagger_ui_engine/logo_small.png
         swagger_ui_engine/favicon-32x32.png
         swagger_ui_engine/favicon-16x16.png
